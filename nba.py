6984ee9de3208aba6324c215f1945e5acd527ed8
felipejf95 <felipejf95@hotmail.com>
2023-06-23 15:58:19 -0300

    teste

--- nba.py
+++ nba.py
@@ -1,3 +1,7 @@
 import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+
+jogadores_url = "https://raw.githubusercontent.com/felipejf95/NBA/main/2021-2022%20NBA%20Player%20Stats%20-%20Regular.csv"
+
+dados_jogadores = pd.read_csv(jogadores_url)
