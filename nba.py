7a50b6bae0e4c9d41d45eb00ef0a02cdbafec8a8
felipejf95 <felipejf95@hotmail.com>
2023-06-24 15:49:04 -0300

    filtragem top 3 lideres de estatisticas

--- nba.py
+++ nba.py
@@ -2,6 +2,20 @@ import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
-player_data_url = "https://raw.githubusercontent.com/felipejf95/NBA/main/teste%5B/player_data.csv"
+jogadores_url = "https://raw.githubusercontent.com/felipejf95/NBA/main/players_stats_14-15.csv"
 
-player_data = pd.read_csv(player_data_url)
+jogadores = pd.read_csv(jogadores_url)
+
+
+
+# 5 primeiras linhas
+#print(jogadores.head()) 
+# dados do arquivo
+#print(jogadores.info())
+
+lider_pontos = jogadores.nlargest(3, 'PTS')
+lider_assistencias = jogadores.nlargest(3, 'AST')
+lider_rebotes = jogadores.nlargest(3, 'REB')
+print (lider_pontos[['Name', 'Games Played', 'PTS', 'AST', 'REB', 'FG%', 'Team', ]])
+print(lider_assistencias[['Name', 'Games Played', 'PTS', 'AST', 'REB', 'FG%', 'Team', ]])
+print(lider_rebotes[['Name', 'Games Played', 'PTS', 'AST', 'REB', 'FG%', 'Team', ]])
