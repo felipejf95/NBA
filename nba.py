d1f6724bd0b809f48d34dcf698955a6c0889babb
felipejf95 <felipejf95@hotmail.com>
2023-06-24 17:22:58 -0300

    base de estatisticas dos times

--- nba.py
+++ nba.py
@@ -13,9 +13,20 @@ jogadores = pd.read_csv(jogadores_url)
 # dados do arquivo
 #print(jogadores.info())
 
+# Top 3 Lideres de Pontos, Rebotes e Assistencias
+
 lider_pontos = jogadores.nlargest(3, 'PTS')
 lider_assistencias = jogadores.nlargest(3, 'AST')
 lider_rebotes = jogadores.nlargest(3, 'REB')
-print (lider_pontos[['Name', 'Games Played', 'PTS', 'AST', 'REB', 'FG%', 'Team', ]])
-print(lider_assistencias[['Name', 'Games Played', 'PTS', 'AST', 'REB', 'FG%', 'Team', ]])
-print(lider_rebotes[['Name', 'Games Played', 'PTS', 'AST', 'REB', 'FG%', 'Team', ]])
+#print (lider_pontos[['Name', 'Games Played', 'PTS', 'AST', 'REB', 'FG%', 'Team', ]])
+#print(lider_assistencias[['Name', 'Games Played', 'PTS', 'AST', 'REB', 'FG%', 'Team', ]])
+#print(lider_rebotes[['Name', 'Games Played', 'PTS', 'AST', 'REB', 'FG%', 'Team', ]])
+
+array = lider_pontos.head()
+p1 = array.values[0]
+p2 = array.values[1]
+print (p1[3])
+print (p2[3])
+
+
+
